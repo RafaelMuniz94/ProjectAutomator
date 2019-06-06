6c4a452cfcb4a738d31bae9cfa0f418fd14347e0
Rafael Muniz <raruznh@gmail.com>
2019-06-06 00:50:45 -0300

    Improvements in GithubController and Folder Creation

--- Controller/GithubController.py
+++ Controller/GithubController.py
@@ -7,18 +7,50 @@ password = ""
 class GithubController:
 
     def get_User_Login(self):
-        user = Github(userName,password).get_user()
-        return user
+        if self.user is None:
+            self.user = Github(userName,password).get_user()
+           
+        return self.user
 
     def createRepo(self):
         user = self.get_User_Login()
-        repo = user.create_repo(self.name,self.description)
-        return repo.clone_url
 
+        try:
+            self.__repo = user.get_repo(self.name)
+            self.__error = True
+            self.__message = 'Repositório já existe!'
+            self.__url = self.__repo.clone_url
+        except:
+            self.__repo = user.create_repo(self.name,self.description)
+            self.__url=  self.__repo.clone_url
+
+
+    @property
+    def Error(self):
+        return self.__error
+
+    @property
+    def Message(self):
+        return self.__message
+
+    @property
+    def URL(self):
+        return self.__url
+    @property
+    def CommitUrl(self):
+        return self.__repo.commits_url
+
+    def CloneUrl(self):
+        return self.__repo.clone_url
 
 
     def  __init__(self,name,description):
         self.name = name
         self.description = description
+        self.user = None
+        self.__url = None
+        self.__error= False
+        self.__message= None
+        self.__repo = None
         
 
--- Controller/PythonCreator.py
+++ Controller/PythonCreator.py
@@ -4,6 +4,35 @@ from GithubController import GithubController
 
 name = sys.argv[1]
 description = sys.argv[2]
+path = sys.argv[3]
 
-git = GithubController(name,description)
-print(git.createRepo())
+def createGitHub():
+    git = GithubController(name,description)
+    git.createRepo()
+    if git.Error:
+        print(git.Message)
+        print(git.CloneUrl)
+        print(git.CommitUrl)
+        return False
+    else:
+        print(git.URL)
+        print(git.CloneUrl)
+        print(git.CommitUrl)
+        return True
+
+def createFolder():
+    os.mkdir(path)
+
+def createReadMe():
+    filepath = path + "/README.md"
+    readme = open(filepath,"w")
+    readme.write(Text())
+
+
+def Text():
+    text = "Auto generated project structure by [ProjectAutomator](https://github.com/RafaelMuniz94/ProjectAutomator)"
+    return text
+
+createGitHub()
+createFolder()
+createReadMe()
